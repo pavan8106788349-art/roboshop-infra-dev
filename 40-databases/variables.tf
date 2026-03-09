df145b475e0a6aa65b1c147be48d6a63bf082c46
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-03-09 11:17:20 +0530

    terraform

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z05813963UHKM0FRNKXB"
+}
+
+variable "domain_name" {
+    default="daws88s.space"
+}
