134e07dddf35ab61a7808617355d64bcd3dc6549
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-03-09 14:37:29 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 component=$1
+environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
-git clone https://github.com/pavan8106788349-art/ansible-roboshop-roles-tf.git 
+git clone https://github.com/pavan8106788349-art/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment roboshop.yaml
