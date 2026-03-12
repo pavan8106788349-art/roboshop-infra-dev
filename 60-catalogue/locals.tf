2e1d8f447be002f46117f31fcf7b585bfdf042ed
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-03-12 20:30:56 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,7 @@
 locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id = data.aws_ami.joindevops.id
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
    common_tags = {
         Project = var.project
         Environment = var.environment
