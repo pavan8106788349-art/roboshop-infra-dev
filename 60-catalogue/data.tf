e0b72134f588ddc32fa855bf6fe74bea8ca957cf
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-03-12 20:30:02 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -23,6 +23,6 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 }
 
 
-data "aws_ssm_parameter" "private_subnet_id" {
-    name = "/${var.project}/${var.environment}/private_subnet_id"
+data "aws_ssm_parameter" "private_subnet_ids" {
+    name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
