0a87364294c9264cf7e87b919fbe626e6ef1d947
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-03-13 14:19:08 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -22,7 +22,7 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
       name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
 
-data "aws_ssm_parameter" "vpc_id_id" {
+data "aws_ssm_parameter" "vpc_id" {
       name = "/${var.project}/${var.environment}/vpc_id"
 }
 
