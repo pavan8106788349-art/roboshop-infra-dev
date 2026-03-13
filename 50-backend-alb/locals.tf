78340aa6fe2abfb910b247a60707d7bd1221ae13
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-03-13 17:21:54 +0530

    terraform

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -1,7 +1,6 @@
 locals {
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
    common_tags = {
         Project = var.project
         Environment = var.environment
