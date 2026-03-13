4b255c3ec502332a084da812947eb09c6e874788
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-03-13 17:08:15 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -30,6 +30,6 @@ data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
-data "aws_ssm_parameter" "backend_alb_listener_arn" {
+data "aws_ssm_parameter" "backend_alb_listener_sg" {
     name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
 }
