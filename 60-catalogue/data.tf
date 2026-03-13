50f4c0b37d05f4a0b140da7077d28645cb6366df
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-03-13 14:21:36 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -26,6 +26,6 @@ data "aws_ssm_parameter" "vpc_id" {
       name = "/${var.project}/${var.environment}/vpc_id"
 }
 
-data "aws_ssm_parameter" "private_subnet_id" {
-    name = "/${var.project}/${var.environment}/private_subnet_id"
+data "aws_ssm_parameter" "private_subnet_ids" {
+    name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
