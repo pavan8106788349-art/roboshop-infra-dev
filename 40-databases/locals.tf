bd70777e6485c93d40da6529e3346394b1d3ddb3
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-07-09 15:44:00 +0530

    terraform files

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -5,7 +5,7 @@ locals {
         Environment = var.environment
         Terraform = "true"
     }
-    
+
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
@@ -13,8 +13,6 @@ locals {
      mysql_role_name = join("-", [
             for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
         ])
-    mysql_policy_name = join("", [
-            for name in ["${var.project}","${var.environment}", "mysql"] : title(name)
-        ])
+
 
 }
