b501dae5d9493aead8ced9d15ff60a7c94a4c80c
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-07-09 16:03:29 +0530

    terraform files

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -24,3 +24,9 @@ resource "aws_iam_role" "mysql" {
     local.common_tags
   )
 }  
+
+resource "aws_iam_policy" "mysql" {
+  name        = local.mysql_policy_name
+  description = "A policy for MySQL Ec2 instance"
+  policy      = file("mysql-iam-policy.json")
+}
