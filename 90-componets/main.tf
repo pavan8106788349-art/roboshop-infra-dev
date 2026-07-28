f91ac52b6d0508bbb4218931a1b08445716dcce1
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-07-28 15:21:53 +0530

    changed files

--- 90-componets/main.tf
+++ 90-componets/main.tf
@@ -1,6 +1,8 @@
 module "component" {
-    for_each = var.components
-    source = "git:://https://github.com/pavan8106788349-art/terraform-roboshop-component.git?ref=main"
-    component = each.key
-    rule_priority = each.value.rule_priority
+  for_each = var.components
+
+  source = "git::https://github.com/pavan8106788349-art/terraform-roboshop-component.git?ref=main"
+
+  component     = each.key
+  rule_priority = each.value.rule_priority
 }
